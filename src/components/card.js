40fbc577e5e628915591a91a3442b2a75dc7caa4
otabek05 <109728366+otabek05@users.noreply.github.com>
2022-08-05 17:25:57 +0900

    style and card parts have

--- src/components/card.js
+++ src/components/card.js
@@ -8,9 +8,9 @@ const Cards = ({ item, handleClick }) => {
         <img src={img} alt="" />
       </div>
       <div className="details">
+        <p>Price: $ {price}</p>
         <p>{title}</p>
         <p>{author}</p>
-        <p>Price - {price}Rs</p>
         <button onClick={() => handleClick(item)}>Add to Cart</button>
       </div>
     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useInsertionEffect, useState } from "react";
 import "../styles/navbar.css";
+import list from "../data";
 
 const Navbar = ({ setShow, size }) => {
+  const [search,setSearch]=useState('')
   return (
     <nav>
       <div className="nav_box">
         <span className="my_shop" onClick={() => setShow(true)}>
           My Shoping
         </span>
+        <div>
+          <span><input className="inp" type={'search'} placeholder='Search'></input></span>
+          
+        </div>
         <div className="cart" onClick={() => setShow(false)}>
           <span>
             <i className="fas fa-cart-plus"></i>
